a5f7d05004e114b3b8d2c73d9c894594fee4ee13
arthurFerraz9 <arthurmf9@gmail.com>
2018-10-27 19:57:14 -0300

    Adiciona predicatidos relativo a parte inicial do texto

--- prolog/test.pl
+++ prolog/test.pl
@@ -1,20 +1,48 @@
-investimento(governo,ensinoMilitar).
-investimento(governo,pesquisa).
-investimento(governo,ensinoMedio).
-investimento(governo,john,mary).
-investimento(iniciativaPrivada,ensinoSuperior).
-investimento(governo,policiaMilitar).
-investimento(governo,turismo).
+/*Proposta de convenção: tudo que for proposta do candidato colocar no infinitivo, e tudo que ele afirma colocar no presente*/
 
-poder(policiaMilitar).
-poder(policiaCivil).
-poder(militares).
+investir(governo,ensinoMilitar).
+investir(governo,pesquisa).
+investir(governo,ensinoMedio).
+investir(iniciativaPrivada,ensinoSuperior).
+investir(governo,policiaMilitar).
+investir(governo,turismo).
 
-reformulacao(secretariaSeguranca).
-reformulacao(sistemaPenitenciario).
+darPoder(policiaMilitar).
+darPoder(policiaCivil).
+darPoder(militares).
 
-combate(corrupcao).
-combate(sonegacao).
+reformular(secretariaSeguranca).
+reformular(sistemaPenitenciario).
 
-causa(crise,corrupcao).
-causa(crise,sonegacao).
+combater(corrupcao).
+combater(sonegacao).
+
+causa(corrupcao, crise).
+causa(sonegacao, crise).
+
+reestruturar(administracaoPublica).
+
+naoPriorizar(X, Y) :- setor(X), setor(Y).
+/*Pensar em alguma forma de relacionar setor com serviço */
+retiraDinheiro(corrupcao,X) :- servico(X),essencial(X).
+
+sofre(estado,corrupcao).
+
+deturpa(corrupcao, politicasPublicas).
+retiraDinheiro(corrupcao, X) :- servicoEssencial(X).
+
+estaAbandonado(saude).
+estaAbandonado(educacao).
+estaAbandonado(segurancaPublica).
+
+desastre(UPP).
+atrapalha(policiaParticular, planejamentoAtividadePolicial).
+concorreInvestigacao(secretariaSegurancaPublica, policiaCivil).
+
+extinguir(secretariaSegurancaPublica).
+criar(secretariaPoliciaCivil).
+criar(secretariaPoliciaMilitar).
+
+
+
+
