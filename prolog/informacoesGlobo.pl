959754a59a04b334dbf08e511d89e76dbb10d5ca
Leonardo Schimpf Machado <leonardo.schimpf@gmail.com>
2018-10-28 20:31:45 -0300

    Inclusao de primeira pergunta

--- prolog/informacoesGlobo.pl
+++ prolog/informacoesGlobo.pl
@@ -1,5 +1,7 @@
 /* debate globo */
 investir(iniciativaPrivada,presal).
+investir(governo,ensinoTecnico).
+investir(governo,escolasMilitares).
 
 deveSer(municipio,responsavel,educacaoInfantil).
 
@@ -7,7 +9,6 @@ deveReceber(auxilio,iniciativaPrivada,saudePublica).
 
 combater(trafico,auxilia,seguranca).
 
-investir(governo,ensinoTecnico).
 
 
 /* entrevista globo */
@@ -35,8 +36,24 @@ iraConduzir(gabineteDeSeguranca,secretarioPoliciaCivil).
 
 assistencia(qualquerPolicial,autoDeResistencia).
 
-investir(governo,escolasMilitares).
-transforma(escolasPublicas,escolasMilitares).
-
 cobrar(universidadesPublicas).
 manter(cotas).
+
+/* Prestar atenção a partir daqui, que foi onde realizei a primeira pergunta: ehSolucao(transformar(escolasPublicas, escolasMilitares))*/
+proposta(transformar(escolasPublicas,escolasMilitares)).
+
+/* Proposta para o que é uma solução de um problema político, geralmente podemos usar como template para uma pergunta,
+ * ou seja, perguntamos à máquina ehSolucao(proposta) */
+ehSolucao(X) :- proposta(X), X, melhora(vida, pessoas).
+
+/* Senso comum 
+ * Fonte para custos de escolas militares: https://politica.estadao.com.br/noticias/eleicoes,estudantes-de-colegio-militar-custam-tres-vezes-mais-ao-pais,70002473230
+ * Fonte para falta de dinheiro do estado: https://g1.globo.com/rio-de-janeiro/noticia/rj-espera-deficit-de-r-10-bi-em-2018-metade-do-previsto-antes-do-regime-de-recuperacao-fiscal.ghtml
+ * É senso comum de que é necessário investimento em áreas básicas como saúde, educação e transporte urbano */
+falta(dinheiro, estado).
+necessario(investimentos, areasBasicas).
+transformar(escolasPublicas,escolasMilitares) :- multiplicaCusto(estudantes, 3).
+melhora(vida, pessoas) :- not(cortaInvestimentos(areasBasicas)).
+multiplicaCusto(estudantes, 3) :- not(falta(dinheiro, estado)) ; cortaInvestimentos(areasBasicas).
+cortaInvestimentos(areasBasicas) :- not(necessario(investimentos, areasBasicas)).
+
