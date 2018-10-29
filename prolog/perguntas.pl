727b7b94b3c25fcaa0dc2c2c95a3ef7054ac363a
Daniel Ye <danielxinxin1@gmail.com>
2018-10-28 22:52:28 -0300

    adicionando explicacao da pergunta sobre solucoes de dividas

--- prolog/perguntas.pl
+++ prolog/perguntas.pl
@@ -1,8 +1,8 @@
 % p1
 ehSolucao(transformar(escolasPublicas, escolasMilitares)).
 
-%p2
+%p2 
 ehContra(X,Y),recebe(X,Y).
 
-%p3
+%p3 verifica solucoes,verifica o que permite as solucoes, verifica se alguma solucao bloqueia a resolucao de outra
 solucao(A,B),solucao(X,Y),permitidaPor(Y,Z),bloqueia(B,Z).
